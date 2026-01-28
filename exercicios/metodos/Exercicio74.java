7053f67725dc9c6c4389f3279d955a516c41549c
Yago Souza <yagosouza.1802@gmail.com>
2026-01-27 21:30:38 -0300

    feat: cria estrutura inicial

---
+++ exercicios/metodos/Exercicio74.java
@@ -0,0 +1,13 @@
+/* criar um programa em Java que simula um validador de cadastro de usuário,
+ usando métodos bem separados. */
+
+package exercicios.metodos;
+
+import java.util.Scanner;
+
+public class Exercicio74 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
