52c6990b3056379782e2c0fcab9158be2d329e0d
Yago Souza <yagosouza.1802@gmail.com>
2026-01-29 19:50:18 -0300

    feat: adiciona método menu no main

--- exercicios/poo/classes_e_objetos/exercicio74/Main.java
+++ exercicios/poo/classes_e_objetos/exercicio74/Main.java
@@ -7,8 +7,30 @@ Atributos:
 
 package exercicios.poo.classes_e_objetos.exercicio74;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+
+    public static int menu(Scanner scanner) {
+        System.out.println("==============================");
+        System.out.println("          Biblioteca");
+        System.out.println("==============================");
+        System.out.println("[ 1 ] Cadastrar livro \n[ 2 ] Emprestar livro \n[ 2 ] Devolver livro \n[ 4 ] Mostrar info \n[ 5 ] Sair");
+        System.out.println("==============================");
         
+        int escolha;
+        do {
+            System.out.print("Qual opção desejada: ");
+            escolha = scanner.nextInt();
+            if (escolha < 1 || escolha > 5) {
+                System.out.printf("Error: %d não é uma opção válida.%n", escolha);
+            }
+        } while (escolha < 1 || escolha > 5);
+        System.out.println("==============================");
+        return escolha;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
     }
 }
