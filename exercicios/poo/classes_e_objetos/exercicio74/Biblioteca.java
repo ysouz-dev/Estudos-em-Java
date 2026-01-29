3ff9d1fadbdc068d06aafe84ea07b0ec93f7c87f
Yago Souza <yagosouza.1802@gmail.com>
2026-01-29 20:46:00 -0300

    feat: adiciona leitura e validação de título no cadastrarLivro

--- exercicios/poo/classes_e_objetos/exercicio74/Biblioteca.java
+++ exercicios/poo/classes_e_objetos/exercicio74/Biblioteca.java
@@ -1,10 +1,28 @@
 package exercicios.poo.classes_e_objetos.exercicio74;
 
+import java.util.Scanner;
+
 public class Biblioteca {
     Livro[] listaDeLivros = new Livro[0];
     
-    public static Livro CadastrarLivro() {
+    public static void CadastrarLivro(Scanner scanner) {
+        scanner.nextLine();
         Livro livroNovo = new Livro();
-        return livroNovo;
+
+        do {
+            System.out.print("Título do livro: ");
+            livroNovo.titulo = scanner.nextLine();
+
+            if (!validaTitulo(livroNovo.titulo)) {
+                System.out.println("Error: o título deve ter pelo menos 2 caracteres.");
+            }
+
+        } while (!validaTitulo(livroNovo.titulo));
+
+        
+    }
+
+    public static boolean validaTitulo(String titulo) {
+        return titulo.length() > 2;
     }
 }
