c0dfd59a2a2b9fec67b723a0ed5ab4401455f3b2
Yago Souza <yagosouza.1802@gmail.com>
2026-01-29 20:25:52 -0300

    feat: adiciona método cadastrarLivro

--- exercicios/poo/classes_e_objetos/exercicio74/Biblioteca.java
+++ exercicios/poo/classes_e_objetos/exercicio74/Biblioteca.java
@@ -2,5 +2,9 @@ package exercicios.poo.classes_e_objetos.exercicio74;
 
 public class Biblioteca {
     Livro[] listaDeLivros = new Livro[0];
-        
+    
+    public static Livro CadastrarLivro() {
+        Livro livroNovo = new Livro();
+        return livroNovo;
+    }
 }
