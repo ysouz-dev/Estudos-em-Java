d725d6e4f15d9b1804ae719f35340000f2064982
Yago Souza <yagosouza.1802@gmail.com>
2026-01-29 18:23:32 -0300

    feat: adiciona estrutura incial

---
+++ exercicios/poo/classes_e_objetos/exercicio74/Livro.java
@@ -0,0 +1,9 @@
+package exercicios.poo.classes_e_objetos.exercicio74;
+
+public class Livro {
+    String titulo;
+    String autor;
+    int paginas;
+    boolean emprestado = false;
+    
+}
---
+++ exercicios/poo/classes_e_objetos/exercicio74/Main.java
@@ -0,0 +1,14 @@
+/*Objetivo: Crie um sistema de biblioteca utilizando uma classe só para livros com: 
+Atributos:
+- titulo
+- autor
+- paginas
+- emprestado (boolean)*/
+
+package exercicios.poo.classes_e_objetos.exercicio74;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
