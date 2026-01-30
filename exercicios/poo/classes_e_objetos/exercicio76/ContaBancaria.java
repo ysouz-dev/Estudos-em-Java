33640c7bbd92558d372ac32395f6af94ff2853a6
Yago Souza <yagosouza.1802@gmail.com>
2026-01-30 16:03:43 -0300

    feat: adiciona estrutura inicial

---
+++ exercicios/poo/classes_e_objetos/exercicio76/ContaBancaria.java
@@ -0,0 +1,7 @@
+package exercicios.poo.classes_e_objetos.exercicio76;
+
+public class ContaBancaria {
+    int numeroConta;
+    String nomeTitular;
+    double saldo;
+}
---
+++ exercicios/poo/classes_e_objetos/exercicio76/Main.java
@@ -0,0 +1,22 @@
+/* Crie uma classe de conta bancária.
+Atributos:
+- número da conta
+- nome do titular
+- saldo
+Métodos:
+- depositar valor
+- sacar valor (não permitir se saldo insuficiente)
+- mostrar saldo
+No programa principal:
+- crie uma conta
+- faça depósito
+- faça saque
+- mostre saldo final */
+
+package exercicios.poo.classes_e_objetos.exercicio76;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
