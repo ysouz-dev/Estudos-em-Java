8511fdd54e546caa476f502f2e825a9bd64ebb87
Yago Souza <yagosouza.1802@gmail.com>
2026-01-30 16:21:07 -0300

    feat: adiciona metodo de saque

--- exercicios/poo/classes_e_objetos/exercicio76/ContaBancaria.java
+++ exercicios/poo/classes_e_objetos/exercicio76/ContaBancaria.java
@@ -13,4 +13,17 @@ public class ContaBancaria {
             System.out.printf("Não é possivel depositar R$ %.2f, escolha um valor maior.%n", valor);
         }
     }
+
+    public void sacar(double valor) {
+        if (valor <= this.saldo && valor > 0 ) {
+            this.saldo -= valor;
+            System.out.println("Saque realizado com sucesso!");
+        } else {
+            String erro = "Não é possivel sacar R$ %.2f. escolha um valor maior".formatted(valor);
+            if (valor > this.saldo) {
+                erro = "Saldo insuficiente para este saque.";
+            }
+            System.out.println(erro);
+        }
+    }
 }
