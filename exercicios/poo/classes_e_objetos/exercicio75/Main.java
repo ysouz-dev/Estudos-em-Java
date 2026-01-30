481b42c9bdc08d6324cb1771de5be40a8c64e38e
Yago Souza <yagosouza.1802@gmail.com>
2026-01-30 11:51:42 -0300

    feat: adiciona estrutura inicial do projeto

---
+++ exercicios/poo/classes_e_objetos/exercicio75/Main.java
@@ -0,0 +1,17 @@
+/* Crie uma classe que represente um produto. Ela deve ter:
+- nome
+- preço
+- quantidade em estoque
+Crie métodos para:
+- mostrar os dados do produto
+- calcular o valor total em estoque (preço × quantidade) */
+
+package exercicios.poo.classes_e_objetos.exercicio75;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
