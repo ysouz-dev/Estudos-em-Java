5f197ffdac88258df1fbbeed8213f049f7957432
Yago Souza <yagosouza.1802@gmail.com>
2026-01-29 21:19:55 -0300

    fix: reorganiza e corrige o método cadastrarlivro

--- exercicios/poo/classes_e_objetos/exercicio74/Biblioteca.java
+++ exercicios/poo/classes_e_objetos/exercicio74/Biblioteca.java
@@ -13,34 +13,20 @@ public class Biblioteca {
             System.out.print("Título do livro: ");
             livroNovo.titulo = scanner.nextLine();
 
-            if (!validaTitulo(livroNovo.titulo)) {
+            if (!livroNovo.validaTitulo()) {
                 System.out.println("Error: o título deve ter pelo menos 2 caracteres.");
             }
 
-        } while (!validaTitulo(livroNovo.titulo));
+        } while (!livroNovo.validaTitulo());
 
         do {
             System.out.print("Nome do autor: ");
             livroNovo.autor = scanner.nextLine().strip().toUpperCase();
 
-            if (!validaAutor(livroNovo.autor)) {
+            if (!livroNovo.validaAutor()) {
                 System.out.println("Error: o nome do autor deve conter apenas caracteres alfabéticos.");
             }
 
-        } while (!validaAutor(livroNovo.autor));
-    }
-
-    public static boolean validaTitulo(String titulo) {
-        return titulo.length() > 2;
-    }
-
-    public static boolean validaAutor(String autor) {
-        String autorSemEspaco = autor.replace(" ", "");
-        for (int i = 0; i < autorSemEspaco.length(); i++) {
-            if (!Character.isLetter(autorSemEspaco.charAt(i))) {
-                return false;
-            }
-        }
-        return true;
+        } while (!livroNovo.validaAutor());
     }
 }
