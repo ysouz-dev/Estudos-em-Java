4166d9d375d5fd6aeab42c9568a570ec83eb35d1
Yago Souza <yagosouza.1802@gmail.com>
2026-01-30 16:09:48 -0300

    feat: adiciona metodo de deposito

--- exercicios/poo/classes_e_objetos/exercicio76/ContaBancaria.java
+++ exercicios/poo/classes_e_objetos/exercicio76/ContaBancaria.java
@@ -4,4 +4,13 @@ public class ContaBancaria {
     int numeroConta;
     String nomeTitular;
     double saldo;
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            this.saldo += valor;
+            System.out.println("Depósito realizado com sucesso!");
+        } else {
+            System.out.printf("Não é possivel depositar R$ %.2f, escolha um valor maior.%n", valor);
+        }
+    }
 }
