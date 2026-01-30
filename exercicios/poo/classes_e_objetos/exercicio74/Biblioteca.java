8cb17eef916ceaced74aec18434935c465171184
Yago Souza <yagosouza.1802@gmail.com>
2026-01-29 21:39:07 -0300

    chore: corrige espacamento de linhas do código

--- exercicios/poo/classes_e_objetos/exercicio74/Biblioteca.java
+++ exercicios/poo/classes_e_objetos/exercicio74/Biblioteca.java
@@ -12,32 +12,25 @@ public class Biblioteca {
         do {
             System.out.print("Título do livro: ");
             livroNovo.titulo = scanner.nextLine();
-
             if (!livroNovo.validaTitulo()) {
                 System.out.println("Error: o título deve ter pelo menos 2 caracteres.");
             }
-
         } while (!livroNovo.validaTitulo());
 
         do {
             System.out.print("Nome do autor: ");
             livroNovo.autor = scanner.nextLine().strip().toUpperCase();
-
             if (!livroNovo.validaAutor()) {
                 System.out.println("Error: o nome do autor deve conter apenas caracteres alfabéticos.");
             }
-
         } while (!livroNovo.validaAutor());
 
         do {
             System.out.print("Quantidade de paginas: ");
             livroNovo.paginas = scanner.nextInt();
-
             if (!livroNovo.validarPagina()) {
                 System.out.println("Error: a quantidade de páginas deve ser maior que 0.");
             }
-
-
         } while (!livroNovo.validarPagina());
     }
 
