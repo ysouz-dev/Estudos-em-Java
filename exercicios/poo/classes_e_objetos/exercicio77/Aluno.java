d07028477a664918d3e286136ae92ac58c806448
Yago Souza <yagosouza.1802@gmail.com>
2026-02-02 15:44:52 -0300

    feat: adiciona estrutura inicial

---
+++ exercicios/poo/classes_e_objetos/exercicio77/Aluno.java
@@ -0,0 +1,7 @@
+package exercicios.poo.classes_e_objetos.exercicio77;
+
+public class Aluno {
+    String nome;
+    double nota1;
+    double nota2;
+}
---
+++ exercicios/poo/classes_e_objetos/exercicio77/Main.java
@@ -0,0 +1,21 @@
+/*Crie uma classe aluno.
+Atributos:
+- nome
+- nota1
+- nota2
+Métodos:
+- calcular média
+- verificar se foi aprovado (média ≥ 7)
+- mostrar situação
+No main:
+- crie 3 alunos
+- mostre média e situação de cada um */
+
+package exercicios.poo.classes_e_objetos.exercicio77;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
