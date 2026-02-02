f6c2df9d46cb789a83b23e2a773b4241653cd193
Yago Souza <yagosouza.1802@gmail.com>
2026-02-02 19:44:11 -0300

    feat: adiciona metodo que cadastra alunos e importa a classe Scanner

--- exercicios/poo/classes_e_objetos/exercicio77/Main.java
+++ exercicios/poo/classes_e_objetos/exercicio77/Main.java
@@ -13,9 +13,31 @@ No main:
 
 package exercicios.poo.classes_e_objetos.exercicio77;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public static Aluno[] cadastroAlunos(Scanner scanner) {
+        Aluno[] alunos = new Aluno[3];
+        for (int i = 0; i < alunos.length; i++) {
+            Aluno aluno = new Aluno();
+            System.out.printf("Digite o nome do %d° aluno: ", i+1);
+            aluno.nome = scanner.nextLine();
+
+            System.out.printf("Digite a %d° nota de %s: ", i+1, aluno.nome);
+            aluno.nota1 = scanner.nextDouble();
+
+            System.out.printf("Digite a %d° nota de %s: ", i+1, aluno.nome);
+            aluno.nota2 = scanner.nextDouble();
+            scanner.nextLine();
+
+            alunos[i] = aluno;
+            System.out.println("=============================");
+        }
+        return alunos;
+    }
+
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
     }
 }
