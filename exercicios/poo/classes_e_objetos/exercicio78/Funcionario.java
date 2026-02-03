725fb1aaebf4d66b94179810aab973776a32fdd3
Yago Souza <yagosouza.1802@gmail.com>
2026-02-02 21:18:23 -0300

    feat: adiciona estrutura incial

---
+++ exercicios/poo/classes_e_objetos/exercicio78/Funcionario.java
@@ -0,0 +1,6 @@
+package exercicios.poo.classes_e_objetos.exercicio78;
+
+public class Funcionario {
+    String nome;
+    double salario;
+}
---
+++ exercicios/poo/classes_e_objetos/exercicio78/Main.java
@@ -0,0 +1,13 @@
+/* Objetivo: 
+- Criar uma classe Funcionario com nome e salário, aplicar aumento salarial
+por percentual e exibir um relatório final com os dados atualizados. */
+
+package exercicios.poo.classes_e_objetos.exercicio78;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
