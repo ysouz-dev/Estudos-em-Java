0dccc5fb029f8e607be1d3fff0c3b84fb5bf1d78
Yago Souza <yagosouza.1802@gmail.com>
2026-02-03 19:11:37 -0300

    feat: adiciona construtor

--- exercicios/poo/encapsulamento/exercicio79/Produto.java
+++ exercicios/poo/encapsulamento/exercicio79/Produto.java
@@ -5,6 +5,12 @@ public class Produto {
     private double preco;
     private int estoque;
 
+    public Produto(String nome, double preco, int estoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
     public String getNome() {
         return this.nome;
     }
