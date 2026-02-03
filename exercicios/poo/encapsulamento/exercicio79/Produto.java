7548333b2721541103c0220f3c0d42118bb59a7c
Yago Souza <yagosouza.1802@gmail.com>
2026-02-03 18:41:53 -0300

    feat: adiciona getter e setter de nome

--- exercicios/poo/encapsulamento/exercicio79/Produto.java
+++ exercicios/poo/encapsulamento/exercicio79/Produto.java
@@ -4,4 +4,12 @@ public class Produto {
     private String nome;
     private double preco;
     private int estoque;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String novoNome) {
+        this.nome = novoNome;
+    }
 }
