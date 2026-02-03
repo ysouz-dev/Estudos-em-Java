8f6bde5fff8fae74b115f2d6f9bfbf386d20e95c
Yago Souza <yagosouza.1802@gmail.com>
2026-02-03 18:37:55 -0300

    feat: adiciona estrutura inicial

---
+++ exercicios/poo/encapsulamento/exercicio79/Main.java
@@ -0,0 +1,23 @@
+/* Crie uma classe Produto com:
+- nome
+- preço
+- estoque
+Regras:
+- nome não pode ser vazio
+- preço deve ser maior que zero
+- estoque não pode ser negativo
+Exigências:
+- construtor obrigatório
+- getters para todos os atributos
+- setters com validação
+- método que calcula o valor total em estoque */
+
+package exercicios.poo.encapsulamento.exercicio79;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
---
+++ exercicios/poo/encapsulamento/exercicio79/Produto.java
@@ -0,0 +1,7 @@
+package exercicios.poo.encapsulamento.exercicio79;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private int estoque;
+}
