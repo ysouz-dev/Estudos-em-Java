91936e17f7b89ae8d11bd46523a5ea3d61c0ceb8
Yago Souza <yagosouza.1802@gmail.com>
2026-02-02 21:41:07 -0300

    feat: adiciona metodo que cadastra funcionarios

--- exercicios/poo/classes_e_objetos/exercicio78/Main.java
+++ exercicios/poo/classes_e_objetos/exercicio78/Main.java
@@ -7,6 +7,24 @@ package exercicios.poo.classes_e_objetos.exercicio78;
 import java.util.Scanner;
 
 public class Main {
+
+    public static Funcionario[] cadastraFuncionarios(Scanner scanner) {
+        Funcionario[] funcionarios = new Funcionario[3];
+        for (int i = 0; i < funcionarios.length; i++) {
+            Funcionario funcionario = new Funcionario();
+            
+            System.out.printf("Digite o nome do %d° funcionário: ", i+1);
+            funcionario.nome = scanner.nextLine();
+
+            System.out.printf("Digite o salário de %s: R$ ", funcionario.nome);
+            funcionario.salario = scanner.nextDouble();
+            scanner.nextLine();
+
+            funcionarios[i] = funcionario;
+        }
+        return funcionarios;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
     }
