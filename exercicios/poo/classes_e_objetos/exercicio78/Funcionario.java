4a509e3807c996a56db1c305d4a9b7b28ad3b059
Yago Souza <yagosouza.1802@gmail.com>
2026-02-02 21:32:26 -0300

    feat: adiciona metodo que imprime dados do fucionario

--- exercicios/poo/classes_e_objetos/exercicio78/Funcionario.java
+++ exercicios/poo/classes_e_objetos/exercicio78/Funcionario.java
@@ -8,4 +8,10 @@ public class Funcionario {
         this.salario += percentual / 100 * this.salario;
         System.out.printf("Parabens pelo aumento %s!%n", this.nome);
     }
+
+    public void mostrarDados() {
+        System.out.println("=================");
+        System.out.printf("Nome: %s %nSalário: R$ %.2f%n", this.nome, this.salario);
+        System.out.println("=================");
+    }
 }
