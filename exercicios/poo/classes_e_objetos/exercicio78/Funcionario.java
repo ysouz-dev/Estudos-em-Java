12c082f3aa63237ecdfcdb4ea2cb0205a9059c6b
Yago Souza <yagosouza.1802@gmail.com>
2026-02-02 21:28:55 -0300

    feat: adiciona metodo que aumenta salario

--- exercicios/poo/classes_e_objetos/exercicio78/Funcionario.java
+++ exercicios/poo/classes_e_objetos/exercicio78/Funcionario.java
@@ -3,4 +3,9 @@ package exercicios.poo.classes_e_objetos.exercicio78;
 public class Funcionario {
     String nome;
     double salario;
+
+    public void aumentarSalario (int percentual) {
+        this.salario += percentual / 100 * this.salario;
+        System.out.printf("Parabens pelo aumento %s!%n", this.nome);
+    }
 }
