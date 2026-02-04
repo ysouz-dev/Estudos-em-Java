32a735c518483a6614ed99659d7307d3596059bf
Yago Souza <yagosouza.1802@gmail.com>
2026-02-04 16:51:50 -0300

    feat: adiciona validacoes no metodo construtor

--- exercicios/poo/encapsulamento/exercicio81/Aluno.java
+++ exercicios/poo/encapsulamento/exercicio81/Aluno.java
@@ -6,6 +6,9 @@ public class Aluno {
     private double nota2;
 
     public Aluno(String nome, double nota1, double nota2) {
+        if (!validaNome(nome)) throw new IllegalArgumentException("Nome informado inválido!");
+        if (!validaNota(nota1) || !validaNota(nota2)) throw new IllegalArgumentException("Nota informada inválida!");
+
         this.nome = nome;
         this.nota1 = nota1;
         this.nota2 = nota2;
