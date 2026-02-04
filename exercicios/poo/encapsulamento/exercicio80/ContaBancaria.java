ec646c5024572c548a6d77c39e16d693ef0ab3bc
Yago Souza <yagosouza.1802@gmail.com>
2026-02-04 14:58:57 -0300

    feat: adiciona atributo de classe contador de contas

--- exercicios/poo/encapsulamento/exercicio80/ContaBancaria.java
+++ exercicios/poo/encapsulamento/exercicio80/ContaBancaria.java
@@ -1,6 +1,8 @@
 package exercicios.poo.encapsulamento.exercicio80;
 
 public class ContaBancaria {
+    private static int contadorDeContas = 0;
+
     private int conta;
     private String titular;
     private double saldo;
