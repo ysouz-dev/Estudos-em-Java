5ffc683522e93096bd70590ab62dd20ad44645e4
Yago Souza <yagosouza.1802@gmail.com>
2026-02-04 16:25:17 -0300

    feat: adiciona estrutura inicial

---
+++ exercicios/poo/encapsulamento/exercicio81/Aluno.java
@@ -0,0 +1,7 @@
+package exercicios.poo.encapsulamento.exercicio81;
+
+public class Aluno {
+    private String nome;
+    private double nota1;
+    private double nota2;
+}
---
+++ exercicios/poo/encapsulamento/exercicio81/Main.java
@@ -0,0 +1,27 @@
+/* Crie uma classe Aluno com: 
+Atributos privados:
+- nome
+- nota1
+- nota2
+Regras:
+- nome válido (não vazio, só letras, mínimo 3 caracteres)
+- notas entre 0 e 10
+Métodos:
+- construtor obrigando a passar nome, nota1 e nota2
+- getters e setters com validação
+- método calcularMedia() → retorna a média
+- método situacao() → retorna “APROVADO” se média ≥7, caso contrário “REPROVADO”
+Extra:
+- no main, crie um array de 3 alunos
+- peça os dados ao usuário usando Scanner
+- depois mostre nome, notas, média e situação de cada aluno*/
+
+package exercicios.poo.encapsulamento.exercicio81;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+    }
+}
