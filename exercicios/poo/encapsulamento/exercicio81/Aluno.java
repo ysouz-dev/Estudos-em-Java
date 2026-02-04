efc38c1cad1aa7a2a7eb0afac70fa4409ef5b056
Yago Souza <yagosouza.1802@gmail.com>
2026-02-04 16:26:38 -0300

    feat: adiciona construtor

--- exercicios/poo/encapsulamento/exercicio81/Aluno.java
+++ exercicios/poo/encapsulamento/exercicio81/Aluno.java
@@ -4,4 +4,10 @@ public class Aluno {
     private String nome;
     private double nota1;
     private double nota2;
+
+    public Aluno(String nome, double nota1, double nota2) {
+        this.nome = nome;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+    }
 }
