7143f50c52e08e42853d80be2a95fc4d265a9ea4
Yago Souza <yagosouza.1802@gmail.com>
2026-02-04 13:44:46 -0300

    feat: adiciona estrutura inicial

---
+++ exercicios/poo/encapsulamento/exercicio80/ContaBancaria.java
@@ -0,0 +1,7 @@
+package exercicios.poo.encapsulamento.exercicio80;
+
+public class ContaBancaria {
+    private int conta;
+    private String titular;
+    private double saldo;
+}
---
+++ exercicios/poo/encapsulamento/exercicio80/Main.java
@@ -0,0 +1,23 @@
+/* Crie uma classe ContaBancaria com:
+- número da conta
+- titular
+- saldo
+Regras:
+- saldo inicial não pode ser negativo
+- saque não pode deixar saldo negativo
+- depósito só aceita valores positivos
+Exigências:
+- saldo NÃO pode ter setter direto
+- getter para saldo
+- métodos depositar e sacar
+- construtor validando estado inicial */
+
+package exercicios.poo.encapsulamento.exercicio80;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
