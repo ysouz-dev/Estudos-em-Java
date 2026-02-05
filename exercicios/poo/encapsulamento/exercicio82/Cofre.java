98edb6a920d8183871ff16f3cd6536e3f52c4bc2
Yago Souza <yagosouza.1802@gmail.com>
2026-02-05 14:41:33 -0300

    feat: adiciona metodo construtor

--- exercicios/poo/encapsulamento/exercicio82/Cofre.java
+++ exercicios/poo/encapsulamento/exercicio82/Cofre.java
@@ -3,4 +3,8 @@ package exercicios.poo.encapsulamento.exercicio82;
 public class Cofre {
     private String senha;
     private boolean aberto;
+
+    public Cofre(String senha) {
+        this.senha = senha;
+    }
 }
