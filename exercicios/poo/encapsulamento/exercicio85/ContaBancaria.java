c12d9a65306e368b2580fffc927c01312de01cab
Yago Souza <yagosouza.1802@gmail.com>
2026-02-10 13:15:03 -0300

    feat: adiciona metodo construtor

--- exercicios/poo/encapsulamento/exercicio85/ContaBancaria.java
+++ exercicios/poo/encapsulamento/exercicio85/ContaBancaria.java
@@ -3,4 +3,11 @@ package exercicios.poo.encapsulamento.exercicio85;
 public class ContaBancaria {
     private double saldo;
     private double limiteDiario;
+
+    public ContaBancaria() {
+        this.saldo = 0;
+        this.limiteDiario = 1000;
+    }
+
+
 }
