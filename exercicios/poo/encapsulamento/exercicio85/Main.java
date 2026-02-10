075367fd102fa0fbb2e4d52c04dbf9e240adf147
Yago Souza <yagosouza.1802@gmail.com>
2026-02-10 15:20:05 -0300

    feat: adiciona fluxo principal no main

--- exercicios/poo/encapsulamento/exercicio85/Main.java
+++ exercicios/poo/encapsulamento/exercicio85/Main.java
@@ -11,6 +11,38 @@ package exercicios.poo.encapsulamento.exercicio85;
 
 public class Main {
     public static void main(String[] args) {
+        ContaBancaria conta = new ContaBancaria();
+        conta.getResumo();
+        System.out.println("--------");
+
+        conta.depositar(1500);
+        conta.getResumo();
+        System.out.println("--------");
         
+        conta.sacar(100);
+        conta.getResumo();
+        System.out.println("--------");
+
+        conta.sacar(900);
+        conta.getResumo();
+        System.out.println("--------");
+
+        conta.reajustarLimiteDiario(2000);
+        conta.getResumo();
+        System.out.println("--------");
+
+        conta.depositar(2600);
+        conta.sacar(1000);
+        conta.getResumo();
+        System.out.println("--------");
+
+        conta.virarDia();
+        conta.getResumo();
+        System.out.println("--------");
+
+        conta.sacar(2000);
+        conta.getResumo();
+
+        conta.sacar(100);
     }
 }
