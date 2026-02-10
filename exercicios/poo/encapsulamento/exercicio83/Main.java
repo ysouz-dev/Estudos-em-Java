770d70d833894327d21693651f79ea89913824bd
Yago Souza <yagosouza.1802@gmail.com>
2026-02-09 22:37:06 -0300

    feat: adiciona fluxo principal no main

--- exercicios/poo/encapsulamento/exercicio83/Main.java
+++ exercicios/poo/encapsulamento/exercicio83/Main.java
@@ -12,6 +12,20 @@ package exercicios.poo.encapsulamento.exercicio83;
 
 public class Main {
     public static void main(String[] args) {
-        
+        ContaSimples conta1 = new ContaSimples("Yago Souza");
+        conta1.informacoes();
+
+        conta1.depositar(1500);
+        System.out.println("---------------------------");
+        System.out.println("Depósito realizado!");
+        conta1.informacoes();
+
+        conta1.sacar(950);
+        System.out.println("---------------------------");
+        System.out.println("Saque realizado!");
+        conta1.informacoes();
+
+        conta1.alterarNome("Nome aleatório");
+        conta1.informacoes();
     }
 }
