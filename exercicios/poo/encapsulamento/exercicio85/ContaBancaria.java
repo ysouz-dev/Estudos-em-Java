e804244accfa822f22c03cb75de658115d019fd2
Yago Souza <yagosouza.1802@gmail.com>
2026-02-10 14:06:03 -0300

    feat: implementa interface na classe

--- exercicios/poo/encapsulamento/exercicio85/ContaBancaria.java
+++ exercicios/poo/encapsulamento/exercicio85/ContaBancaria.java
@@ -1,6 +1,6 @@
 package exercicios.poo.encapsulamento.exercicio85;
 
-public class ContaBancaria {
+public class ContaBancaria implements Conta {
     private double saldo;
     private double limiteDiario;
 
@@ -11,5 +11,30 @@ public class ContaBancaria {
     
     private static boolean validaValor(double valor) {
         return valor >= 0;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        if (!validaValor(valor)) {
+            throw new IllegalArgumentException("Valor de depósito inválido!");
+        }
+        this.saldo += valor;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        if (!validaValor(valor) || valor > this.saldo || valor > limiteDiario) {
+            String erro = "Valor de saque inválido!";
+
+            if (valor > this.saldo) {
+                erro = "Saldo insuficiente para esse saque.";
+            } else if (limiteDiario == 0) {
+                erro = "Valor desejado superior ao limite diário de saque."; 
+            }
+
+            throw new IllegalArgumentException(erro);
+        }
+        
+
     }
 }
