09663a27215c9a890fa6e135aa8a24d66fcc73e2
Yago Souza <yagosouza.1802@gmail.com>
2026-02-13 02:36:45 -0300

    feat: adiciona getter de pagamentos confirmados do evento

--- exercicios/poo/encapsulamento/exercicio86/Evento.java
+++ exercicios/poo/encapsulamento/exercicio86/Evento.java
@@ -40,6 +40,16 @@ public class Evento {
         return contador;
     }
 
+    public int getPagamentosConfirmados() {
+        int contador = 0;
+        for (Inscricao inscritos : this.listaDeInscricoes) {
+            if (inscritos.getPagamento()) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
     public void realizarInscricao(String nome, String email) {
         if (!validaVagas(totalVagas)) {
             throw new IllegalStateException("Número máximo de vagas atingido!");
