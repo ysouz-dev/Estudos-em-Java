d68c66fb0a318e9404a228b6252ea5e3da82605b
Yago Souza <yagosouza.1802@gmail.com>
2026-02-13 03:44:46 -0300

    feat: adiciona fluxo princial ao main

--- exercicios/poo/encapsulamento/exercicio86/Main.java
+++ exercicios/poo/encapsulamento/exercicio86/Main.java
@@ -2,6 +2,25 @@ package exercicios.poo.encapsulamento.exercicio86;
 
 public class Main {
     public static void main(String[] args) {
+        Evento evento1 = new Evento("Confraternização", 2);
+        evento1.realizarInscricao("yago", "teste@gmail.com");
+        evento1.realizarInscricao("beto", "teste2@gmail.com");
+        evento1.mostraResumoEvento();
+        System.out.println("=====================");
 
+        evento1.confirmarPagamento("teste12@gmail.com");
+        evento1.confirmarPagamento("teste@gmail.com");
+        evento1.mostraResumoEvento();
+        System.out.println("======================");
+
+        evento1.cancelarInscricao("teste2@gmail.com");
+        evento1.mostraResumoEvento();
+        System.out.println("======================");
+
+        evento1.realizarInscricao("thiago", "teste3@gmail.com");
+        evento1.listarInscritosAtivos();
+
+        // teste de cadastro com limite atingido
+        evento1.realizarInscricao("arthur", "teste4@gmail.com");
     }
 }
