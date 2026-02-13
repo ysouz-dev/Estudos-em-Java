49efeab3c2e9adf1b8c783db91d0c515b8294e1c
Yago Souza <yagosouza.1802@gmail.com>
2026-02-13 00:20:49 -0300

    feat: adiciona metodo que valida nome

--- exercicios/poo/encapsulamento/exercicio86/Inscricao.java
+++ exercicios/poo/encapsulamento/exercicio86/Inscricao.java
@@ -12,4 +12,17 @@ public class Inscricao {
         this.pagamentoConfirmado = false;
         this.ativa = false;
     }
+
+    public static boolean validaNome(String nome) {
+        if (nome.isEmpty() || nome.length() < 3)
+            return false;
+
+        String nomeSemEspaco = formataNome(nome).replace(" ", "");
+        for (int i = 0; i < nomeSemEspaco.length(); i++) {
+            if (!Character.isLetter(nomeSemEspaco.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
