9bb72d4130a22350a0cc50d3acd556e2a0fc9171
Yago Souza <yagosouza.1802@gmail.com>
2026-02-22 22:53:32 -0300

    feat: adiciona metodo que cancela assinatura no sistema

--- projetos/gerenciamento_de_assinaturas/service/Sistema.java
+++ projetos/gerenciamento_de_assinaturas/service/Sistema.java
@@ -27,4 +27,8 @@ public class Sistema {
         listaDeAssinaturas.add(assinatura);
     }
 
+    public void cancelarAssinatura(Assinatura assinatura) {
+        assinatura.cancelarAssinatura();
+    }
+
 }
