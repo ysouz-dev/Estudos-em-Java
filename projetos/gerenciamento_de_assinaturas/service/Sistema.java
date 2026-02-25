0e4d0f4090ec997f51fbeee69e74b4a7be1d539e
Yago Souza <yagosouza.1802@gmail.com>
2026-02-25 20:08:56 -0300

    fix: remove metodo sem uso

--- projetos/gerenciamento_de_assinaturas/service/Sistema.java
+++ projetos/gerenciamento_de_assinaturas/service/Sistema.java
@@ -60,19 +60,6 @@ public class Sistema {
         assinatura.reativarAssinatura();
     }
 
-    public void listarAssinaturasAtivas() {
-        for (Assinatura ass : this.listaDeAssinaturas) {
-            if (ass.getStatus()) {
-                System.out.println("----------------------");
-                System.out.println("Nome do cliente: " + ass.getCliente().getNome());
-                System.out.println("Plano: " + ass.getPlano().getNomePlano());
-                System.out.println("Status: " + ((ass.getStatus()) ? "ATIVO" : "INATIVO"));
-                System.out.println("Data de início: " + ass.getData());
-            }
-        }
-        System.out.println("----------------------");
-    }
-
     public double faturamentoMensal() {
         double faturamento = 0;
         for (Assinatura ass : this.listaDeAssinaturas) {
