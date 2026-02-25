8a993a8f029d83ce8b04079dfbcd0d4ff1e22926
Yago Souza <yagosouza.1802@gmail.com>
2026-02-25 16:37:49 -0300

    feat: adiciona metodo que cria assinatura no menu

--- projetos/gerenciamento_de_assinaturas/controller/Menu.java
+++ projetos/gerenciamento_de_assinaturas/controller/Menu.java
@@ -1,6 +1,7 @@
 package projetos.gerenciamento_de_assinaturas.controller;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 import projetos.gerenciamento_de_assinaturas.model.*;
 import projetos.gerenciamento_de_assinaturas.service.Sistema;
 
@@ -62,5 +63,61 @@ public class Menu {
 
         sistema.cadastrarPlano(new Plano(nomePlano, valor, limite));
         System.out.println("Plano cadastrado com sucesso!");
+    }
+
+    public void criarAssinatura() {
+        if (this.sistema.getCliente().size() > 0 && this.sistema.getPlano().size() > 0) {
+            System.out.println("===== Assinatura =====");
+            int indexEmail;
+            do {
+                System.out.print("Email de quem deseja cadastrar: ");
+                String email = this.scanner.nextLine();
+                indexEmail = findEmail(this.sistema.getCliente(), email);
+
+                if (indexEmail < 0) {
+                    System.out.println("Email não encontrado, tente novamente.");
+                }
+            } while (indexEmail < 0);
+
+            listaDePlanos();
+            System.out.println("==============");
+            int opcaoPlano;
+            do {
+                System.out.print("Qual plano deseja assinar: ");
+                opcaoPlano = this.scanner.nextInt();
+
+                if (opcaoPlano < 1 || opcaoPlano > this.sistema.getPlano().size()) {
+                    System.out.println("Erro: %d não é uma opção válida.".formatted(opcaoPlano));
+                }
+            } while (opcaoPlano < 1 || opcaoPlano > this.sistema.getPlano().size());
+
+            this.sistema.criarAssinatura(
+                    new Assinatura(this.sistema.getCliente().get(indexEmail),
+                            this.sistema.getPlano().get(opcaoPlano - 1)));
+            System.out.println("Assinatura cadastrada com sucesso!");
+        } else {
+            String erro = "O sistema não possui nenhum cliente cadastrado para criar assinatura";
+            if (this.sistema.getPlano().size() == 0) {
+                erro = "O sistema não possui nenhum plano cadastrado para criar assinatura";
+            }
+            System.out.println("============================");
+            System.out.println(erro);
+        }
+    }
+
+    private void listaDePlanos() {
+        for (int i = 0; i < this.sistema.getPlano().size(); i++) {
+            System.out.println("[%d] %s - R$ %.2f".formatted(i + 1, this.sistema.getPlano().get(i).getNomePlano(),
+                    this.sistema.getPlano().get(i).getValorMensalidade()));
+        }
+    }
+
+    private static int findEmail(ArrayList<Cliente> lista, String email) {
+        for (int i = 0; i < lista.size(); i++) {
+            if (email.strip().toUpperCase().equals(lista.get(i).getEmail())) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
