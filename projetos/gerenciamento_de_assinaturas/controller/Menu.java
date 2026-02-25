08c5f19687b25d4c8dd6e1fdeabb112fe2e949ca
Yago Souza <yagosouza.1802@gmail.com>
2026-02-25 12:45:28 -0300

    feat: adiciona metodo que inicia o menu

--- projetos/gerenciamento_de_assinaturas/controller/Menu.java
+++ projetos/gerenciamento_de_assinaturas/controller/Menu.java
@@ -8,4 +8,28 @@ public class Menu {
     public Menu() {
         this.scanner = new Scanner(System.in);
     }
+
+    public int iniciar() {
+        System.out.println("============================");
+        System.out.println("==       Assinaturas      ==");
+        System.out.println("============================");
+        System.out.println("[ 1 ] Cadastrar cliente");
+        System.out.println("[ 2 ] Cadastrar plano");
+        System.out.println("[ 3 ] Criar assinatura");
+        System.out.println("[ 4 ] Cancelar assinatura");
+        System.out.println("[ 5 ] Reativar assinatura");
+        System.out.println("[ 6 ] Listar assinaturas ativas");
+        System.out.println("[ 7 ] Calcular faturamento mensal");
+        System.out.println("[ 8 ] Encerrar");
+        System.out.println("============================");
+        int opcao;
+        do {
+            System.out.print("Digite a opção desejada: ");
+            opcao = this.scanner.nextInt();
+            if (opcao < 1 || opcao > 8) {
+                System.out.println("Erro: %d não é uma opção válida.".formatted(opcao));
+            }
+        } while (opcao < 1 || opcao > 8);
+        return opcao;
+    }
 }
