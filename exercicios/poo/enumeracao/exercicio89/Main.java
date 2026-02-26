f1dfd115f3263e407bae289e66a51fa33124e559
Yago Souza <yagosouza.1802@gmail.com>
2026-02-26 20:42:50 -0300

    feat: adiciona fluxo principal no main

--- exercicios/poo/enumeracao/exercicio89/Main.java
+++ exercicios/poo/enumeracao/exercicio89/Main.java
@@ -11,6 +11,25 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        Jogador j1 = new Jogador("yago");
+        Jogador j2 = new Jogador("lucas");
+
+        Partida partida = new Partida(j1, j2);
+        partida.iniciarPartida();
+        partida.finalizarPartida();
+        System.out.println("------------------------");
+        System.out.println("Placar: %s %d x %d %s".formatted(partida.getNomeJogador1(), partida.getPontoJogador1(),
+                partida.getPontoJogador2(), partida.getNomeJogador2()));
+        System.out.println("Rounds: " + partida.getRounds());
+        System.out.println("Vencedor: " + partida.nomeVencedor()); // vencedor é escolhido de forma aleatoria
+        System.out.println("------------------------");
+        mostrarStatusJogador(j1);
+        System.out.println("------------------------");
+        mostrarStatusJogador(j2);
+        System.out.println("------------------------");
+
+        // testa finalizar a mesma partida novamente
+        partida.finalizarPartida();
 
     }
 }
