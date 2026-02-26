eb94b19b429e49e2b15fb9dcf2fed0f3f7b2d0b3
Yago Souza <yagosouza.1802@gmail.com>
2026-02-26 14:18:58 -0300

    feat: adiciona metodo que valida nome

--- exercicios/poo/enumeracao/exercicio88/Pedido.java
+++ exercicios/poo/enumeracao/exercicio88/Pedido.java
@@ -19,4 +19,17 @@ public class Pedido {
         this.listaItens = new ArrayList<String>();
         this.status = Status.PENDENTE;
     }
+
+    private static boolean validaNome(String nome) {
+        if (nome == null || nome.strip().isEmpty() || nome.length() < 3) {
+            return false;
+        }
+        String nomeSemEspaco = nome.replace(" ", "");
+        for (int i = 0; i < nomeSemEspaco.length(); i++) {
+            if (!Character.isLetter(nomeSemEspaco.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
