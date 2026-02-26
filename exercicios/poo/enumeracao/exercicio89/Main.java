c5bfa8bd9f264fda57053dd34b4c0dd6ae53f54a
Yago Souza <yagosouza.1802@gmail.com>
2026-02-26 20:29:43 -0300

    feat: adiciona metodo que mostra resumo do jogador

--- exercicios/poo/enumeracao/exercicio89/Main.java
+++ exercicios/poo/enumeracao/exercicio89/Main.java
@@ -3,6 +3,13 @@
 package exercicios.poo.enumeracao.exercicio89;
 
 public class Main {
+    public static void mostrarStatusJogador(Jogador jogador) {
+        System.out.println("Nome: " + jogador.getNome());
+        System.out.println("Vitorias: " + jogador.getVitoria());
+        System.out.println("Derrotas: " + jogador.getDerrota());
+        System.out.println("Empate: " + jogador.getEmpate());
+    }
+
     public static void main(String[] args) {
 
     }
