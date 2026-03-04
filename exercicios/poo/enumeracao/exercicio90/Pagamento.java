d38bc60ffadddad1dc5ca7593d5adc4358afe1a9
Yago Souza <yagosouza.1802@gmail.com>
2026-03-04 11:56:52 -0300

    feat: adiciona construtor a classe pagamento

--- exercicios/poo/enumeracao/exercicio90/Pagamento.java
+++ exercicios/poo/enumeracao/exercicio90/Pagamento.java
@@ -4,4 +4,10 @@ public class Pagamento {
     private double valorPagamento;
     private MetodoPagamento metodoPagamento;
     private StatusPagamento statusPagamento;
+
+    public Pagamento(double valorPagamento, String metodoPagamento) {
+        this.valorPagamento = valorPagamento;
+        this.metodoPagamento = metodoPagamento;
+        this.statusPagamento = StatusPagamento.PENDENTE;
+    }
 }
