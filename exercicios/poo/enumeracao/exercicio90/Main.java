0c37bafd7c325c035a347eff5736ba1c2aa46dc2
Yago Souza <yagosouza.1802@gmail.com>
2026-03-04 16:53:53 -0300

    feat: adiciona fluxo principal ao main

--- exercicios/poo/enumeracao/exercicio90/Main.java
+++ exercicios/poo/enumeracao/exercicio90/Main.java
@@ -12,6 +12,24 @@ package exercicios.poo.enumeracao.exercicio90;
 
 public class Main {
     public static void main(String[] args) {
+        Pagamento pagamento1 = new Pagamento(100, "pix");
+        System.out.println("---------- Resumo pagamento 1 ----------");
+        pagamento1.resumoPagamento();
+        pagamento1.realizarPagamento();
+
+        Pagamento pagamento2 = new Pagamento(50, "crédito");
+        System.out.println("---------- Resumo pagamento 2 ----------");
+        pagamento2.resumoPagamento();
+        pagamento2.cancelarPagamento();
+
+        System.out.println("---------- Resumo pagamento 1 ----------");
+        pagamento1.resumoPagamento();
+
+        System.out.println("---------- Resumo pagamento 2 ----------");
+        pagamento2.resumoPagamento();
+
+        // tenta realizar pagamento ja cancelado/processado
+        pagamento1.realizarPagamento();
 
     }
 }
