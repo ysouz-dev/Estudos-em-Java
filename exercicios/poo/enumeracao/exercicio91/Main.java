5617da390f751602093e64a0ac6d5433806e233c
Yago Souza <yagosouza.1802@gmail.com>
2026-03-11 13:37:27 -0300

    feat: adiciona metodo de menu principal

--- exercicios/poo/enumeracao/exercicio91/Main.java
+++ exercicios/poo/enumeracao/exercicio91/Main.java
@@ -11,7 +11,43 @@ Regras:
 
 package exercicios.poo.enumeracao.exercicio91;
 
+import exercicios.poo.enumeracao.exercicio91.Semaforo.Status;
+import java.util.Scanner;
+
 public class Main {
+
+    public static int menuPrincipal(Semaforo semaforo, Scanner scanner) {
+        System.out.println("----------------");
+        System.out.println("Cor atual: " + semaforo.getStatus());
+
+        if (semaforo.getStatus() == Status.VERMELHO) {
+            System.out.println("Pare!!");
+        } else if (semaforo.getStatus() == Status.AMARELO) {
+            System.out.println("Reduza a velocidade!");
+        } else {
+            System.out.println("Prossiga!");
+        }
+        if (semaforo.tempoRestante() > 0) {
+            System.out.println("Tempo restante: " + semaforo.tempoRestante() + " segundos");
+        } else {
+            System.out.println("Tempo restante: 0 segundos");
+        }
+        System.out.println("----------------");
+        System.out.println("[ 1 ] Avançar estado do sinal");
+        System.out.println("[ 2 ] Atualizar tempo restante");
+        System.out.println("[ 0 ] Parar semáforo");
+        System.out.println("----------------");
+        int opcao;
+        do {
+            System.out.print("Digite uma opção: ");
+            opcao = scanner.nextInt();
+            if (opcao < 0 || opcao > 2) {
+                System.out.println("Error: %d não é uma opção válida!".formatted(opcao));
+            }
+        } while (opcao < 0 || opcao > 2);
+        return opcao;
+    }
+
     public static void main(String[] args) {
 
     }
