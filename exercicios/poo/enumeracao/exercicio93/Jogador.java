a4b0f061bb8022ee5c83f31edf27a02af7a3293b
Yago Souza <yagosouza.1802@gmail.com>
2026-03-12 13:52:08 -0300

    feat: adiciona construtor a classe jogador

--- exercicios/poo/enumeracao/exercicio93/Jogador.java
+++ exercicios/poo/enumeracao/exercicio93/Jogador.java
@@ -4,4 +4,10 @@ public class Jogador {
     private String nome;
     private int pontuacao;
     private Rank rank;
+
+    public Jogador(String nome) {
+        this.nome = nome.strip().toUpperCase();
+        this.pontuacao = 0;
+        this.rank = Rank.BRONZE;
+    }
 }
