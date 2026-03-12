38093169097a81dac0bee3427c014d20043ff4bb
Yago Souza <yagosouza.1802@gmail.com>
2026-03-12 15:39:25 -0300

    feat: adiciona fluxo principal no main

--- exercicios/poo/enumeracao/exercicio93/Main.java
+++ exercicios/poo/enumeracao/exercicio93/Main.java
@@ -13,6 +13,22 @@ package exercicios.poo.enumeracao.exercicio93;
 
 public class Main {
     public static void main(String[] args) {
+        Jogador j1 = new Jogador("Yago");
+        j1.resumo();
+        System.out.println("---");
 
+        j1.adicionarPontos(1000);
+        j1.resumo();
+        System.out.println("---");
+
+        j1.adicionarPontos(1000);
+        j1.resumo();
+        System.out.println("---");
+
+        j1.adicionarPontos(1000);
+        j1.resumo();
+
+        // testa adicionar pontos negativos
+        j1.adicionarPontos(-1);
     }
 }
