c29de7336b25518623adc757fdb4e9ef76fcb3b2
Yago Souza <yagosouza.1802@gmail.com>
2026-03-15 22:53:24 -0300

    feat: adiciona construtor da classe Pedido

--- exercicios/poo/enumeracao/exercicio94/Pedido.java
+++ exercicios/poo/enumeracao/exercicio94/Pedido.java
@@ -17,4 +17,13 @@ public class Pedido {
     private TipoEntrega tipoEntrega;
     private int estimativaEntrega;
 
+    public Pedido(TipoEntrega tipoEntrega) {
+        this.idPedido = ("#" + LocalDate.now().getYear() + "0" + id);
+        id++;
+        this.diaCriacao = LocalDate.now().getDayOfMonth();
+        this.mesCriacao = LocalDate.now().getMonthValue();
+        this.anoCriacao = LocalDate.now().getYear();
+        this.tipoEntrega = tipoEntrega;
+
+    }
 }
