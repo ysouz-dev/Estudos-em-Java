728e2443bdd1849560d8e48d3c722045639587ad
Yago Souza <yagosouza.1802@gmail.com>
2026-03-18 15:36:05 -0300

    feat: adiciona metodo que valida nome a classe funcionario

--- exercicios/poo/heranca/exercicio95/Funcionario.java
+++ exercicios/poo/heranca/exercicio95/Funcionario.java
@@ -8,4 +8,16 @@ public abstract class Funcionario {
         this.nome = nome.strip().toUpperCase();
         this.salarioBase = salarioBase;
     }
+
+    private static void validaNome(String nome) {
+        if (nome == null || nome.strip().isEmpty() || nome.strip().length() < 3) {
+            throw new IllegalArgumentException("Nome inválido!");
+        }
+        String nomeSemEspaco = nome.replace(" ", "");
+        for (int i = 0; i < nomeSemEspaco.length(); i++) {
+            if (!Character.isLetter(nomeSemEspaco.charAt(i))) {
+                throw new IllegalArgumentException("Nome inválido!");
+            }
+        }
+    }
 }
