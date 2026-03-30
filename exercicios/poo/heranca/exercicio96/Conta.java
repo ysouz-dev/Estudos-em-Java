58b5870ffff0078955a9f469aa767d4fde94cf06
Yago Souza <yagosouza.1802@gmail.com>
2026-03-30 12:56:35 -0300

    feat: adiciona construtor a classe conta

--- exercicios/poo/heranca/exercicio96/Conta.java
+++ exercicios/poo/heranca/exercicio96/Conta.java
@@ -5,4 +5,9 @@ import java.math.BigDecimal;
 public abstract class Conta {
     private String titular;
     private BigDecimal saldo;
+
+    public Conta(String titular) {
+        this.titular = titular.strip().toUpperCase();
+        this.saldo = BigDecimal.ZERO;
+    }
 }
