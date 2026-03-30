2739ecb90066c2b1cb4ce90ac52e1af270f05260
Yago Souza <yagosouza.1802@gmail.com>
2026-03-30 12:55:08 -0300

    feat: adiciona estrutura inicial

---
+++ exercicios/poo/heranca/exercicio96/Conta.java
@@ -0,0 +1,8 @@
+package exercicios.poo.heranca.exercicio96;
+
+import java.math.BigDecimal;
+
+public abstract class Conta {
+    private String titular;
+    private BigDecimal saldo;
+}
---
+++ exercicios/poo/heranca/exercicio96/Main.java
@@ -0,0 +1,20 @@
+/* Crie:
+- Uma classe base Conta
+- Classes derivadas como ContaCorrente e ContaPoupanca
+- Um enum StatusConta para representar o estado da conta
+Regras:
+- Contas podem estar ATIVAS ou BLOQUEADAS
+- Não é permitido sacar valores de contas bloqueadas
+- Cada tipo de conta pode ter regras diferentes para saque
+O sistema deve permitir:
+- Depositar
+- Sacar
+- Validar operações com base no status */
+
+package exercicios.poo.heranca.exercicio96;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
