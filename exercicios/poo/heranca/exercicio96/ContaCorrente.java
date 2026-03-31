4839cec9d274fd963f8ffe2d7c2ffb1ac37fb869
Yago Souza <yagosouza.1802@gmail.com>
2026-03-31 16:11:07 -0300

    feat: adiciona classe conta corrente com taxa ao sacar

---
+++ exercicios/poo/heranca/exercicio96/ContaCorrente.java
@@ -0,0 +1,18 @@
+package exercicios.poo.heranca.exercicio96;
+
+import java.math.BigDecimal;
+
+public class ContaCorrente extends Conta {
+    private BigDecimal taxaSaque;
+
+    public ContaCorrente(String titular) {
+        super(titular);
+        this.taxaSaque = new BigDecimal("0.02");
+    }
+
+    @Override
+    public void sacar(BigDecimal valor) {
+        BigDecimal valorDaTaxa = valor.multiply(this.taxaSaque);
+        super.sacar(valor.add(valorDaTaxa));
+    }
+}
