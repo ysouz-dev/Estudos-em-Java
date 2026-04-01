764331a8a93f9c791101a880aa37a3c805dcbc73
Yago Souza <yagosouza.1802@gmail.com>
2026-04-01 14:21:58 -0300

    feat: adiciona metodo que valida nome do personagem

--- exercicios/poo/heranca/exercicio97/Personagem.java
+++ exercicios/poo/heranca/exercicio97/Personagem.java
@@ -5,7 +5,15 @@ public abstract class Personagem {
     private int vida;
 
     public Personagem(String nome) {
+        validaNome(nome);
+
         this.nome = nome.strip().toUpperCase();
         this.vida = 100;
+    }
+
+    private static void validaNome(String nome) {
+        if (nome == null || nome.isBlank() || nome.strip().length() < 3) {
+            throw new IllegalArgumentException("Nome de personagem inválido!");
+        }
     }
 }
