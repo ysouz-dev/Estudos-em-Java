add002e3d62b6561b195a51e75089e0fdff8a86f
Yago Souza <yagosouza.1802@gmail.com>
2026-04-01 13:36:31 -0300

    feat: adiciona fluxo principal ao main

--- exercicios/poo/heranca/exercicio96/Main.java
+++ exercicios/poo/heranca/exercicio96/Main.java
@@ -13,8 +13,31 @@ O sistema deve permitir:
 
 package exercicios.poo.heranca.exercicio96;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
+        ContaCorrente contacc = new ContaCorrente("Yago");
+        ContaPoupanca contacp = new ContaPoupanca("Yan");
+        contacc.depositar(new BigDecimal(100));
+        contacp.depositar(new BigDecimal(100));
+        contacc.resumo();
+        System.out.println("------------------");
+        contacp.resumo();
+        System.out.println("------------------");
+
+        contacc.sacar(new BigDecimal(50));
+        contacp.sacar(new BigDecimal(50));
+        contacc.resumo();
+        System.out.println("------------------");
+        contacp.resumo();
+        System.out.println("------------------");
+
+        contacp.sacar(new BigDecimal(50));
+        contacp.resumo();
+
+        // teste sacar 49 da conta corrente sem considerar a taxa
+        contacc.sacar(new BigDecimal(49));
 
     }
 }
