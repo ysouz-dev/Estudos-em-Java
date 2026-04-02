654db09487d680c267212e2e02c2a81ef18346c6
Yago Souza <yagosouza.1802@gmail.com>
2026-04-02 02:05:27 -0300

    docs: adiciona enunciado do exercicio

--- exercicios/poo/heranca/exercicio97/Main.java
+++ exercicios/poo/heranca/exercicio97/Main.java
@@ -1,5 +1,15 @@
+/* Crie:
+- Uma classe base Personagem
+- Classes derivadas como Guerreiro, Mago e Arqueiro
+- Cada personagem deve implementar seu próprio comportamento de ataque.
+O sistema deve:
+- Armazenar os personagens em uma lista (ArrayList)
+- Fazer todos os personagens executarem um ataque */
+
 package exercicios.poo.heranca.exercicio97;
 
 public class Main {
+    public static void main(String[] args) {
 
+    }
 }
