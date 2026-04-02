39980d15959d1830761cd4bd6bdc05220a83d171
Yago Souza <yagosouza.1802@gmail.com>
2026-04-02 02:19:32 -0300

    feat: adiciona a classe guerreiro

---
+++ exercicios/poo/heranca/exercicio97/Guerreiro.java
@@ -0,0 +1,29 @@
+package exercicios.poo.heranca.exercicio97;
+
+public class Guerreiro extends Personagem {
+    private int ataqueForte;
+    private boolean estamina;
+
+    public Guerreiro(String nome) {
+        super(nome);
+        this.ataqueForte = 15;
+        this.estamina = true;
+    }
+
+    @Override
+    public void atacar(Personagem personagem) {
+        if (!this.estamina) {
+            throw new IllegalStateException("O guerreiro precisa recuperar estamina para atacar.");
+        }
+        super.atacar(personagem);
+        reduzirVida(this.ataqueForte);
+        this.estamina = false;
+    }
+
+    public void recuperarEstamina() {
+        if (this.estamina) {
+            throw new IllegalStateException("O guerreiro já possui estamina!");
+        }
+        this.estamina = true;
+    }
+}
