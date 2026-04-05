fe1d814de26883366f85432474654825fc53a512
Yago Souza <yagosouza.1802@gmail.com>
2026-04-05 19:11:48 -0300

    feat: adiciona estrutura inicial da subclasse Aluno

---
+++ exercicios/poo/heranca/exercicio99/Aluno.java
@@ -0,0 +1,23 @@
+package exercicios.poo.heranca.exercicio99;
+
+import java.time.LocalDate;
+
+public class Aluno extends Pessoa {
+    public enum Status {
+        ATIVO,
+        TRANCADO,
+        FORMADO;
+    }
+
+    private static int idGeralMatricula = 1;
+    private final String matricula;
+    private Status status;
+
+    public Aluno(String nome, int idade) {
+        super(nome, idade);
+        this.matricula = ("#" + LocalDate.now().getYear() + idGeralMatricula);
+        idGeralMatricula++;
+        this.status = Status.ATIVO;
+    }
+
+}
