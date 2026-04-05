51fa10071477f44e35fd68d86cedbb4a4690f962
Yago Souza <yagosouza.1802@gmail.com>
2026-04-05 18:28:53 -0300

    feat: adiciona fluxo principal ao main

--- exercicios/poo/heranca/exercicio98/Main.java
+++ exercicios/poo/heranca/exercicio98/Main.java
@@ -9,8 +9,43 @@ Regras:
 
 package exercicios.poo.heranca.exercicio98;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        PedidoNormal pedido1 = new PedidoNormal();
+        PedidoInternacional pedido2 = new PedidoInternacional();
+        ArrayList<Pedido> listaPedidos = new ArrayList<Pedido>();
+        listaPedidos.add(pedido1);
+        listaPedidos.add(pedido2);
 
+        // mostra resumo de cada pedido
+        for (Pedido pedido : listaPedidos) {
+            pedido.resumo();
+            System.out.println("-------------------");
+        }
+
+        // envia cada pedido
+        for (Pedido pedido : listaPedidos) {
+            pedido.enviarPedido();
+        }
+
+        // entrega somente o pedido de id 1001
+        for (Pedido pedido : listaPedidos) {
+            if (pedido.getId() == 1001) {
+                pedido.entregarPedido();
+            }
+        }
+
+        // mostra resumo de cada pedido
+        for (Pedido pedido : listaPedidos) {
+            pedido.resumo();
+            System.out.println("---------------");
+        }
+
+        // testa entregar os dois pedidos com um já entregue
+        for (Pedido pedido : listaPedidos) {
+            pedido.entregarPedido();
+        }
     }
 }
