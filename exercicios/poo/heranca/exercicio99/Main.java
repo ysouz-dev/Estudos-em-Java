a14974f20d7a5dfb0a35432836e7e3cf23a154d3
Yago Souza <yagosouza.1802@gmail.com>
2026-04-05 21:25:06 -0300

    feat: adiciona fluxo principal ao main

--- exercicios/poo/heranca/exercicio99/Main.java
+++ exercicios/poo/heranca/exercicio99/Main.java
@@ -9,8 +9,58 @@ Regras:
 
 package exercicios.poo.heranca.exercicio99;
 
+import java.util.ArrayList;
+import java.math.BigDecimal;
+
+import exercicios.poo.heranca.exercicio99.Professor.Disciplina;
+
 public class Main {
     public static void main(String[] args) {
+        Aluno aluno1 = new Aluno("Yago", 23);
+        Aluno aluno2 = new Aluno("Joao", 24);
+        Professor professor1 = new Professor("vanessa", 40, Disciplina.MATEMATICA, new BigDecimal("3000"));
+        Professor professor2 = new Professor("Orioli", 35, Disciplina.HISTORIA, new BigDecimal("2810"));
 
+        ArrayList<Pessoa> listaGeral = new ArrayList<Pessoa>();
+        listaGeral.add(aluno1);
+        listaGeral.add(aluno2);
+        listaGeral.add(professor1);
+        listaGeral.add(professor2);
+
+        // mostra resumo da lista
+        for (Pessoa pessoa : listaGeral) {
+            pessoa.resumo();
+            System.out.println("---------------");
+        }
+
+        // forma os dois alunos e coloca os dois professores para dar aula
+        for (Pessoa pessoa : listaGeral) {
+            if (pessoa.getClass().getSimpleName().equals("Aluno")) {
+                ((Aluno) pessoa).formar();
+            } else {
+                ((Professor) pessoa).iniciarAula();
+            }
+        }
+
+        // aumenta R$ 300 do salário dos professores
+        for (Pessoa pessoa : listaGeral) {
+            if (pessoa.getClass().getSimpleName().equals("Professor")) {
+                ((Professor) pessoa).aumentarSalario(new BigDecimal("300"));
+            }
+        }
+
+        // faz aniversario de todos da lista
+        for (Pessoa pessoa : listaGeral) {
+            pessoa.fazerAniversário();
+        }
+
+        // mostra resumo de todos novamente
+        for (Pessoa pessoa : listaGeral) {
+            pessoa.resumo();
+            System.out.println("=====================");
+        }
+
+        // testa trancar a matricula de um aluno formado
+        ((Aluno) listaGeral.get(0)).trancarMatricula();
     }
 }
