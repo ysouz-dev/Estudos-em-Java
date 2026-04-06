4acfaa7d378b7f57b107a5738d24df551d3184fe
Yago Souza <yagosouza.1802@gmail.com>
2026-04-06 01:36:42 -0300

    feat: adiciona metodo que verifica se a entrada da partida eletrica e valida

--- exercicios/poo/heranca/exercicio100/Moto.java
+++ exercicios/poo/heranca/exercicio100/Moto.java
@@ -5,12 +5,19 @@ public class Moto extends Veiculo {
         SIM,
         NAO;
 
+        public static void isPartidaEletrica(PartidaEletrica partida) {
+            if (partida == null) {
+                throw new IllegalArgumentException("Entrada referente a partida elétrica inválida!");
+            }
+        }
     }
 
     private PartidaEletrica partidaEletrica;
 
     public Moto(String marca, String modelo, double tanque, double consumo, TipoCombustivel combustivel,
             PartidaEletrica partida) {
+
+        PartidaEletrica.isPartidaEletrica(partida);
         super(marca, modelo, tanque, consumo, combustivel);
         this.partidaEletrica = partida;
     }
