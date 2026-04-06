926f7172acf9bcc14fbd0de101d947cb56f7fd36
Yago Souza <yagosouza.1802@gmail.com>
2026-04-06 02:08:34 -0300

    feat: adiciona metodo que valida quantidade de portas

--- exercicios/poo/heranca/exercicio100/Carro.java
+++ exercicios/poo/heranca/exercicio100/Carro.java
@@ -4,7 +4,14 @@ public class Carro extends Veiculo {
     private int portas;
 
     public Carro(String marca, String modelo, double tanque, double consumo, TipoCombustivel combustivel, int portas) {
+        validaPortas(portas);
         super(marca, modelo, tanque, consumo, combustivel);
         this.portas = portas;
+    }
+
+    private static void validaPortas(int portas) {
+        if (portas < 2) {
+            throw new IllegalArgumentException("Um carro precisa ter no mínimo 2 portas.");
+        }
     }
 }
