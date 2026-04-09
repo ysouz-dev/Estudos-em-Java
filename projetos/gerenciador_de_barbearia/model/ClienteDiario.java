464bce6deba1f0efcd1c12b7f65a6dd1310f49a0
Yago Souza <yagosouza.1802@gmail.com>
2026-04-08 21:08:37 -0300

    feat: adiciona subclasse ClienteDiario

---
+++ projetos/gerenciador_de_barbearia/model/ClienteDiario.java
@@ -0,0 +1,15 @@
+package projetos.gerenciador_de_barbearia.model;
+
+public class ClienteDiario extends Pessoa {
+    private int totalAtendimentos;
+
+    public ClienteDiario(String nome, int idade, String cpf, Sexo sexo) {
+        super(nome, idade, cpf, sexo);
+    }
+
+    @Override
+    public void resumo() {
+        super.resumo();
+        System.out.println("Total de atendimentos: " + this.totalAtendimentos);
+    }
+}
