a911615b5c8879529faa32ce25e5ef2ef903557d
Yago Souza <yagosouza.1802@gmail.com>
2026-04-09 13:45:57 -0300

    feat: adiciona metodo que exibe menu principal

--- projetos/gerenciador_de_barbearia/controller/Menu.java
+++ projetos/gerenciador_de_barbearia/controller/Menu.java
@@ -1,5 +1,6 @@
 package projetos.gerenciador_de_barbearia.controller;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public final class Menu {
@@ -7,5 +8,29 @@ public final class Menu {
 
     public Menu() {
         this.scanner = new Scanner(System.in);
+    }
+
+    public int MenuPrincipal() {
+        System.out.println("=============================");
+        System.out.println("==       Barbearia YS      ==");
+        System.out.println("=============================");
+        System.out.println("[ 1 ] Cadastrar Cliente");
+
+        System.out.println("=============================");
+
+        int escolha = Integer.MIN_VALUE;
+        do {
+            try {
+                System.out.print("Digite um número da opção: ");
+                escolha = this.scanner.nextInt();
+                if (escolha < 1 || escolha > 1) {
+                    System.out.printf("Erro: %d não é uma opção válida!%n", escolha);
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Erro: digite um número das opções!");
+                this.scanner.nextLine();
+            }
+        } while (escolha < 1 || escolha > 1);
+        return escolha;
     }
 }
