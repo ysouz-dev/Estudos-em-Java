4934f09ef334487e21d06d009063c6650c763b2c
Yago Souza <yagosouza.1802@gmail.com>
2026-04-09 15:58:39 -0300

    feat: adiciona cadastro de cliente ao fluxo principal no main

--- projetos/gerenciador_de_barbearia/Main.java
+++ projetos/gerenciador_de_barbearia/Main.java
@@ -1,7 +1,23 @@
 package projetos.gerenciador_de_barbearia;
 
+import projetos.gerenciador_de_barbearia.controller.Menu;
+
 public class Main {
     public static void main(String[] args) {
+        Menu menu = new Menu();
+        int escolha = Integer.MIN_VALUE;
+        while (escolha != 0) {
+            escolha = menu.MenuPrincipal();
 
+            switch (escolha) {
+                case 1:
+                    menu.cadastrarCliente();
+                    break;
+
+                case 0:
+                    menu.encerrarSistema();
+                    break;
+            }
+        }
     }
 }
