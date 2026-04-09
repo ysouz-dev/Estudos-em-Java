35090aa5476948f0cd14159ea6e95c0fb246ee14
Yago Souza <yagosouza.1802@gmail.com>
2026-04-08 22:11:15 -0300

    feat: adiciona classe menu

---
+++ projetos/gerenciador_de_barbearia/controller/Menu.java
@@ -0,0 +1,11 @@
+package projetos.gerenciador_de_barbearia.controller;
+
+import java.util.Scanner;
+
+public final class Menu {
+    private Scanner scanner;
+
+    public Menu() {
+        this.scanner = new Scanner(System.in);
+    }
+}
